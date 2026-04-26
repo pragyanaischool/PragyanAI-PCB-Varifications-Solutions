3b25831d9508d57c85b5f7c21d6178893038feed
pragyanaischool <75975970+pragyanaischool@users.noreply.github.com>
2026-04-26 17:00:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,8 @@ from ai.orchestrator import run_full_analysis
 from ui.visualization import show_visualization
 from ui.insights_panel import show_insights_panel
 from ui.chat_panel import show_chat_panel   # ✅ NEW
-
+import sys
+sys.modules['cv2'] = None
 # Utils
 from utils.file import save_uploaded_file, safe_delete
 
